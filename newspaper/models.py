5b71a200a44cd8399e8efc5690ad8125bda31ba7
Rostyslav <rostyslav.furman@gmail.com>
2023-07-24 05:21:14 +0300

    create models and migrations

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,39 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+        ordering = ["username"]
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic,
+                              on_delete=models.CASCADE,
+                              related_name="newspapers")
+    publishers = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name="newspapers")
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -16,7 +16,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "newspaper",
 ]
+
+AUTH_USER_MODEL = "newspaper.Redactor"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -72,7 +76,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
